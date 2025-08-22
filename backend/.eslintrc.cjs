9d35e6aa676d728c37c2fd4b1233f8e4ef827f40
sNirajan <snirajan212@gmail.com>
2025-08-21 21:15:04 -0500

    chore(backend): eslint+prettier setup with on-save formatting

---
+++ backend/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "prettier"
+  ],
+  rules: {
+    "import/order": ["warn", { "alphabetize": { order: "asc", caseInsensitive: true } }]
+  }
+};
